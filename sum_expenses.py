f273d98ce58280b309b8c3b7100bb861cd245105
Ethan Sharp <ethansharpdev@gmail.com>
2022-06-07 17:41:55 -0600

    sum expenses

--- sum_expenses.py
+++ sum_expenses.py
@@ -1,6 +1,6 @@
 total = 0
 expenses = []
-num_expenses = int(input("Enter number of expenses:"))
+num_expenses = int(input("Enter your number of expenses:"))
 
 for i in range(num_expenses):
     expenses.append(float(input("Enter an expense:")))
