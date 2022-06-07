5e3024f8ef2e12c603758669df284a214b72bcc7
Ethan Sharp <ethansharpdev@gmail.com>
2022-06-07 17:35:58 -0600

    rock paper scissors and dice roll games

--- dice_roll_game.py
+++ dice_roll_game.py
@@ -1,3 +1,10 @@
-first_name = 'John'
+import random
 
-print(first_name != 'John')
+roll = random.randint(1,6)
+
+guess = int(input("Guess the dice roll: \n"))
+
+if guess == roll:
+    print(f"Correct! They rolled a {roll}")
+else:
+    print(f"Wrong! They rolled a {roll}")
